914ea25cc283b698ead1b07bcea244072edc7356
tsuigrac <tsuigrac@grinnell.edu>
2020-03-09 11:33:00 -0500

    Tried changing production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  
+  APPLICATIONNAME::Application.configure do
+       config.assets.compile = true
+  end
   # Code is not reloaded between requests.
   config.cache_classes = true
 
