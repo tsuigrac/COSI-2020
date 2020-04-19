9c01dfb736fe31808dbeed8a9935bc46562cce41
Ally-R <ally.rogers.mail@gmail.com>
2020-04-19 15:26:12 -0500

    Seeded community_resources database EXCEPT Education about Immigration Issues section

--- app/controllers/community_resources_controller.rb
+++ app/controllers/community_resources_controller.rb
@@ -1,5 +1,5 @@
 class CommunityResourcesController < ApplicationController
   def new
-    @community_resource = CommunityResource.new(params.require(:community_resource).permit(:category, :name, :link, :email, :address, :contact, :description))
+    @community_resource = CommunityResource.new(params.require(:community_resource).permit(:category, :name, :link, :email, :phone, :address, :contact, :description))
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,7 @@ ActiveRecord::Schema.define(version: 20200417192722) do
     t.string "name"
     t.string "link"
     t.string "email"
+    t.string "phone"
     t.string "address"
     t.string "contact"
     t.string "description"
