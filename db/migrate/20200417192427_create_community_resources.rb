bd1ec2b8e3edac60f1beccdd04df507006da927d
Ally-R <ally.rogers.mail@gmail.com>
2020-04-20 12:01:27 -0500

    CommunityResources database seeded

--- db/migrate/20200417192427_create_community_resources.rb
+++ db/migrate/20200417192427_create_community_resources.rb
@@ -5,6 +5,7 @@ class CreateCommunityResources < ActiveRecord::Migration[5.1]
       t.string :name
       t.string :link
       t.string :email
+      t.string :phone
       t.string :address
       t.string :contact
       t.string :description
